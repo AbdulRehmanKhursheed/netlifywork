c16986106a80c62be038e8af60e090379378288a
Abdul Rehman Khursheed <malik.ali5560@gmail.com>
2021-09-22 06:01:50 -0700

    First

---
+++ functions/getusers.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+ 
+exports.handler = async function (event, context, callback) {
+  const data = await axios("https://jsonplaceholder.typicode.com/posts");
+  console.log(context, "wahab");
+  if (event.httpMethod == "GET") {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        data: data.data[0],
+      }),
+    });
+  } else if (event.httpMethod == "POST") {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        msg: "This is an Netlify lambda basic Api!, Post Request.",
+      }),
+    });
+  } else {
+    callback(null, {
+      statusCode: 200,
+      body: "Some error occured. ",
+    });
+  }
+};
